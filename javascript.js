f3b7aa59a66d9acd0dd28a3b36757230699e9017
GregSCoding <smendag@gmail.com>
2023-01-17 18:35:35 +0100

    Add playRound, playGame to play the game and helper function to check if player input is valid.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
+function isValid(choice){
+    if (choice === "Rock" || choice === "Paper" || choice === "Scissors"){
+        return true
+    }else{
+        return false
+    }
+}
+
+
 function getComputerChoice(){
     let rannum = Math.floor((Math.random()*100));
+    console.log(rannum);
     if (rannum > 66){
         return "Rock"
     }else if (rannum > 33){
@@ -8,5 +18,54 @@ function getComputerChoice(){
         return "Scissors";
     }
 }
+function playRound(p1choice, p2choice){
+    declarePlayerWin = (p1, p2) => console.log("You win. " + p1 + " beats " + p2);
+    declareCompWin = (p1,p2) => console.log("You lose. " + p2 + " beats " + p1);
+    if (p1choice === p2choice){
+        console.log("Draw");
+        return "Draw";
+    }else if (p1choice === "Rock" && p2choice === "Scissors"){
+        declarePlayerWin(p1choice, p2choice);
+        return "Human";
+    }else if (p1choice === "Paper" && p2choice === "Rock"){
+        declarePlayerWin(p1choice,p2choice);
+        return "Human";
+    }else if (p1choice === "Scissors" && p2choice === "Paper"){
+        declarePlayerWin(p1choice,p2choice);
+        return "Human";
+    }else{
+        declareCompWin(p1choice, p2choice);
+        return "Computer";
+    }
+}
 
-console.log(getComputerChoice());
+function playGame(){
+    console.log("It is a console game of rock paper and scissors, have fun!");
+    let humanScore = 0;
+    let compScore = 0;
+    for (let i = 0; i < 5; i++){
+        let playerChoice;
+        while(true){
+            playerChoice = prompt("Enter your choice.");
+            playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
+            if (isValid(playerChoice)){
+                break;
+            }
+            console.log("Please enter a valid choice (rock, paper or scissors capitaliation does not matter)");
+        }
+        let result = playRound(playerChoice, getComputerChoice());
+        if (result === "Human"){
+            humanScore++;
+        }else if (result === "Computer"){
+            compScore++;
+        }
+    }
+    if (humanScore > compScore){
+        console.log("You won! Congratulations. " + compScore + " to " + humanScore);
+    }else if (compScore > humanScore){
+        console.log("Too bad. Computer won. " + compScore + " to " + humanScore);
+    }else{
+        console.log("It's a draw. Very unlikely!")
+    }
+ }
+ playGame();
